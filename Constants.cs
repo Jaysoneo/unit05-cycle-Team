4adc556ebd8da827ce8b5e82857976c4b5efe19e
Jaysoneo <oli21019@byui.edu>
2022-06-27 09:23:16 -0600

    Screen bigger

--- Constants.cs
+++ Constants.cs
@@ -14,8 +14,8 @@ namespace unit05_cycle_Team.Game
     {
         public static int COLUMNS = 40;
         public static int ROWS = 20;
-        public static int CELL_SIZE = 12;
-        public static int MAX_X = 900;
+        public static int CELL_SIZE = 10;
+        public static int MAX_X = 1100;
         public static int MAX_Y = 600;
 
         public static int FRAME_RATE = 15;
